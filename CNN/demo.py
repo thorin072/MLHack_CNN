59435a5cada25f7dca61f5cb07d86639e27e5745
Gorin Nikita <gorinspace@gmail.com>
2020-03-15 02:16:39 +0300

    new demo

--- CNN/demo.py
+++ CNN/demo.py
@@ -1,49 +1,166 @@
-import os
-import sys
-import random
-import math
-import numpy as np
-import skimage.io
-import matplotlib
-import matplotlib.pyplot as plt
-
-# Root directory of the project
-ROOT_DIR = os.path.abspath("../")
-
-# Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn import utils
-import mrcnn.model as modellib
+from mrcnn.config import Config
+from mrcnn import model as modellib
 from mrcnn import visualize
-# Import COCO config
-sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
-import coco
+import mrcnn
+from mrcnn.utils import Dataset
+from mrcnn.model import MaskRCNN
+import numpy as np
+from numpy import zeros
+from numpy import asarray
+import colorsys
+import argparse
+import imutils
+import random
+import cv2
+import os
+import time
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+from keras.models import load_model
+import time
+from os import listdir
+from xml.etree import ElementTree
 
 
-# Directory to save logs and trained model
-MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-
-# Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    utils.download_trained_weights(COCO_MODEL_PATH)
-
-# Directory of images to run detection on
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
-
-class InferenceConfig(coco.CocoConfig):
-    # Set batch size to 1 since we'll be running inference on
-    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+# Класс описания конфигурации сетки
+class CalculoryConfig(Config):
+    # Конфигурационное имя
+    NAME = "Calculory_config"
+ 
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
+ 
+    # все классы продуктов + бэграунд
+    NUM_CLASSES = 100+1
+   
+    # Кол-во шагов за эпоху
+    STEPS_PER_EPOCH = 131
+    
+    # Скорость обучения
+    LEARNING_RATE=0.006
+    
+    # Вероятность обноружения
+    DETECTION_MIN_CONFIDENCE = 0.9
+    
+    # setting Max ground truth instances
+    MAX_GT_INSTANCES=10
 
-config = InferenceConfig()
+class CalculoryDataset(Dataset):
+    # load the dataset definitions
+    def load_dataset(self,dataset_dir, is_train=True):
+        
+        # Все классы, которые будут использованы в обучении
+        self.add_class("dataset", 1, "rise")
+        #self.add_class("dataset", 2, "eels on rice")
+        #self.add_class("dataset", 3, "pilaf")
+        #self.add_class("dataset", 4, "chicken-'n'-egg on rice")
+       # self.add_class("dataset", 5, "pork cutlet on rice")
+
+        counter_class=1
+        # define data locations for images and annotations
+        images_dir = dataset_dir +'\\'+str(counter_class)+'\\'
+        annotations_dir = dataset_dir +'\\'+str(counter_class)+'\\xml'+'\\'
+        
+        # Iterate through all files in the folder to 
+        #add class, images and annotaions
+        for filename in listdir(images_dir):
+          
+            # extract image id
+            image_id = filename[:-4]
+            
+            # setting image file
+            img_path = images_dir + filename
+            
+            # setting annotations file
+            ann_path = annotations_dir + image_id + '.xml'
+            
+            # adding images and annotations to dataset
+            self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path,)
+
+
+# extract bounding boxes from an annotation file
+    def extract_boxes(self, filename):
+        
+        # load and parse the file
+        tree = ElementTree.parse(filename)
+        # get the root of the document
+        root = tree.getroot()
+        # extract each bounding box
+        boxes = list()
+        for box in root.findall('.//bndbox'):
+            xmin = int(box.find('xmin').text)
+            ymin = int(box.find('ymin').text)
+            xmax = int(box.find('xmax').text)
+            ymax = int(box.find('ymax').text)
+            coors = [xmin, ymin, xmax, ymax]
+            boxes.append(coors)
+        
+        # extract image dimensions
+        width = int(root.find('.//size/width').text)
+        height = int(root.find('.//size/height').text)
+        return boxes, width, height
+# load the masks for an image
+    """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+     """
+    def load_mask(self, image_id):
+        # get details of image
+        info = self.image_info[image_id]
+        
+        # define anntation  file location
+        path = info['annotation']
+        
+        # load XML
+        boxes, w, h = self.extract_boxes(path)
+       
+        # create one array for all masks, each on a different channel
+        masks = zeros([h, w, len(boxes)], dtype='uint8')
+        
+        # create masks
+        class_ids = list()
+        for i in range(len(boxes)):
+            box = boxes[i]
+            row_s, row_e = box[1], box[3]
+            col_s, col_e = box[0], box[2]
+            masks[row_s:row_e, col_s:col_e, i] = 1
+            class_ids.append(self.class_names.index('rise'))
+        return masks, asarray(class_ids, dtype='int32')
+# load an image reference"""Return the path of the image."""
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        print(info)
+        return info['path']
+
+
+config = CalculoryConfig()
 config.display()
 
+train_set = CalculoryDataset()
+train_set.load_dataset(os.path.abspath(os.curdir)+'\\DATAEAT', is_train=True)
+train_set.prepare()
+print('Train: %d' % len(train_set.image_ids))
 
-# Create model object in inference mode.
-model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+# prepare test/val set
+test_set = CalculoryDataset()
+test_set.load_dataset(os.path.abspath(os.curdir)+'\\DATAEAT', is_train=False)
+test_set.prepare()
+print('Test: %d' % len(test_set.image_ids))
 
-# Load weights trained on MS-COCO
-model.load_weights(COCO_MODEL_PATH, by_name=True)
+print("Loading Mask R-CNN model...")
+model = modellib.MaskRCNN(mode="training", config=config, model_dir='./')
+
+#load the weights for COCO
+model.load_weights('.\\Mask_RCNN\\mask_rcnn_coco.h5', 
+                   by_name=True, 
+                   exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
+
+## train heads with higher lr to speedup the learning
+model.train(train_set, test_set, learning_rate=2*config.LEARNING_RATE, epochs=5, layers='heads')
+history = model.keras_model.history.history
+
+
+model_path = os.path.abspath(os.curdir)+'\\mask_rcnn_'+ '.' + str(time.time()) + '.h5'
+model.keras_model.save_weights(model_path)
